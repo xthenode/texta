514de4557b62a806d646c5621fa82382d532f54e
xthenode <xthenode@gmail.com>
2020-04-18 10:37:53 -0400

    Added source.

---
+++ source/xos/app/console/network/protocol/http/cgi/language/texta/main.cpp
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2020 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: main.cpp
+///
+/// Author: $author$
+///   Date: 4/17/2020
+///////////////////////////////////////////////////////////////////////
+#include "xos/app/console/network/protocol/http/cgi/language/texta/main.hpp"
+#include "xos/language/texta/processor/xde/implemented.hpp"
+
+namespace xos {
+namespace app {
+namespace console {
+namespace network {
+namespace protocol {
+namespace http {
+namespace cgi {
+namespace language {
+namespace texta {
+
+/// class implemented
+static xos::language::texta::processor::xde::implemented the_processor;
+
+/// class maint
+static main the_main;
+
+} /// namespace texta
+} /// namespace language
+} /// namespace cgi
+} /// namespace http
+} /// namespace protocol
+} /// namespace network
+} /// namespace console
+} /// namespace app
+} /// namespace xos
